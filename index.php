6b9e99cdc7affc987f167a8ffcff6a7a3aec3f0f
ankurrathi1602 <ankurrathi1611@gmail.com>
2019-02-27 00:20:00 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,221 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Online Assessment Portal</title>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="It is a website to assess the skills of the individual.">
+        <meta name="author" content="">
+        <!-- this is core bootstrap css -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <!-- Fontawesome -->
+        <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css" type="text/css">
+        <!-- custom css -->
+        <link rel="stylesheet" href="css/blog-home.css">
+        <style>
+            .grid{
+                border: 1px solid gray;
+                padding: 15px;
+                margin-left: 1px;
+                border-radius: 10px;
+            }
+        </style>
+    </head>
+    <body>
+
+        <nav class="navbar navbar-inverse navbar-fixed-top navb1" role="navigation">
+            <div class="container">
+            <!-- for mobile devices-->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand">Online Assessment</a>
+                </div>
+                
+            <!--navigations -->
+                <div class="collapse navbar-collapse navbar-ex1-collapse">
+                    <ul class="nav navbar-nav side-nav">
+                        <li><a href="index.php"><i class="fa fa-fw fa-home"></i>Home</a></li>
+                        <li><a href=""><i class="fa fa-fw fa-user"></i>Contact</a></li>
+                        <li><a href="">Help<i class="fa fa-fw fa-question"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!--This is the page content-->
+            <div class="container">
+                <div class="row">
+                <div class="col-lg-12 well">
+                    <a href=""><img height="300" width="100%" src="images/LOGO2.png" alt="LOGO"></a>  
+                </div>
+
+                <!-- Main page content-->
+                    <div class="col-lg-8 well">
+                        <h1 class="page-header">
+                        Computer Science
+                        <small>(CSE)</small>
+                        </h1>
+                    <div class="row">
+                        
+                    <div class="col-6">
+                        <div class="card-deck">
+                            <div class="card mb-3">
+                                <img class="card-img-top img-responsive" src="images/javascript.png" alt="Javascript">
+                                    <div class="card-body">
+                                        <h5 class="card-title">FIRST</h5>
+                                        <p class="card-text">Lorem ipsum dolor sit amet
+                                        consectetur adipisicing
+                                        elit. Qui totam laudantium eaque
+                                        ad dolorum quasi id sint ab neque omnis!</p>
+                                        <button type="button" class="btn btn-primary">Know more</button>
+                                    </div>
+                            </div>
+                        
+                            <div class="card mb3">
+                            <img class="card-img-top" width="100" src="images/bootstrap.jpeg" alt="ootstrap">
+                                <div class="card-body">
+                                    <h5 class="card-title">FIRST</h5>
+                                    <p class="card-text">Lorem ipsum dolor sit amet
+                                    consectetur adipisicing
+                                    elit. Qui totam laudantium eaque
+                                    ad dolorum quasi id sint ab neque omnis!</p>
+                                    <button type="button" class="btn btn-primary">Know more</button>
+                                </div>
+                            </div>
+                            <div class="card mb-3">
+                            <img class="card-img-top img-responsive" src="images/javascript.png" alt="Javascript">
+                                <div class="card-body">
+                                    <h5 class="card-title">FIRST</h5>
+                                    <p class="card-text">Lorem ipsum dolor sit amet
+                                    consectetur adipisicing
+                                    elit. Qui totam laudantium eaque
+                                    ad dolorum quasi id sint ab neque omnis!</p>
+                                    <button type="button" class="btn btn-primary">Know more</button>
+                                </div>
+                            </div>
+                            <div class="card mb-3">
+                            <img class="card-img-top img-responsive" src="images/javascript.png" alt="Javascript">
+                                <div class="card-body">
+                                    <h5 class="card-title">FIRST</h5>
+                                    <p class="card-text">Lorem ipsum dolor sit amet
+                                    consectetur adipisicing
+                                    elit. Qui totam laudantium eaque
+                                    ad dolorum quasi id sint ab neque omnis!</p>
+                                    <button type="button" class="btn btn-primary">Know more</button>
+                                </div>
+                            </div>
+                            </div>
+                            </div>
+                    </div>
+                    <div class="row">
+                        
+                        <div class="card text-center col-md-3 grid">
+                        <img class="card-img-top img-responsive" src="images/javascript.png" alt="Javascript">
+                            <div class="card-body">
+                                <h5 class="card-title">FIRST</h5>
+                                <p class="card-text">Lorem ipsum dolor sit amet
+                                 consectetur adipisicing
+                                 elit. Qui totam laudantium eaque
+                                 ad dolorum quasi id sint ab neque omnis!</p>
+                                 <button type="button" class="btn btn-primary">Know more</button>
+                            </div>
+                        </div>
+                    
+                        <div class="card text-center col-md-3 grid">
+                        <img class="card-img-top img-responsive" src="images/javascript.png" alt="Javascript">
+                            <div class="card-body">
+                                <h5 class="card-title">FIRST</h5>
+                                <p class="card-text">Lorem ipsum dolor sit amet
+                                 consectetur adipisicing
+                                 elit. Qui totam laudantium eaque
+                                 ad dolorum quasi id sint ab neque omnis!</p>
+                                 <button type="button" class="btn btn-primary">Know more</button>
+                            </div>
+                        </div>
+                        <div class="card text-center col-md-3 grid">
+                        <img class="card-img-top img-responsive" src="images/javascript.png" alt="Javascript">
+                            <div class="card-body">
+                                <h5 class="card-title">FIRST</h5>
+                                <p class="card-text">Lorem ipsum dolor sit amet
+                                 consectetur adipisicing
+                                 elit. Qui totam laudantium eaque
+                                 ad dolorum quasi id sint ab neque omnis!</p>
+                                 <button type="button" class="btn btn-primary">Know more</button>
+                            </div>
+                        </div>
+                        <div class="card text-center col-md-3 grid">
+                        <img class="card-img-top img-responsive" src="images/javascript.png" alt="Javascript">
+                            <div class="card-body">
+                                <h5 class="card-title">FIRST</h5>
+                                <p class="card-text">Lorem ipsum dolor sit amet
+                                 consectetur adipisicing
+                                 elit. Qui totam laudantium eaque
+                                 ad dolorum quasi id sint ab neque omnis!</p>
+                                 <button type="button" class="btn btn-primary">Know more</button>
+                            </div>
+                        </div>
+                    </div>
+                    
+                     </div>
+                     <!-- This is the sidebar section-->
+                    <div class="col-md-4 one">
+                        <div class="well">
+                            <form action="">
+                            <div class="form-group">
+                                <input type="text" name="input" class="form-control">
+                            </div>
+                            <button type="submit" class="btn btn-primary">Search</button>
+                            </form>
+
+                            
+                        </div>
+                        <div class="well">
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur repellat, iste impedit laborum quisquam at possimus tempore qui cum facere deleniti enim quibusdam ab amet neque vel. Accusamus, molestias eos.
+
+                        </div>
+                        <div class="well">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam beatae consequatur est, quos vel saepe, ipsum dolor aperiam expedita reprehenderit distinctio veniam temporibus ratione autem at quis, pariatur debitis labore?
+                        </div>
+                        <div class="well">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam beatae consequatur est, quos vel saepe, ipsum dolor aperiam expedita reprehenderit distinctio veniam temporibus ratione autem at quis, pariatur debitis labore?
+                        </div>
+                        <div class="well">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam beatae consequatur est, quos vel saepe, ipsum dolor aperiam expedita reprehenderit distinctio veniam temporibus ratione autem at quis, pariatur debitis labore?
+                        </div>
+                        <div class="well">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam beatae consequatur est, quos vel saepe, ipsum dolor aperiam expedita reprehenderit distinctio veniam temporibus ratione autem at quis, pariatur debitis labore?
+                        </div>
+
+                    
+
+                    </div>
+
+                </div>
+            </div>
+        <!--This is the footer-->
+        <div class="container-fluid navb1">
+        <footer class="page-footer">
+            <div class="row">
+                <div class="col">
+                    <p class="footer">Copyright &copy; 2019 by <a href="">Anukriti</a></p>
+                </div>
+
+            </div>
+        </footer>
+        </div>
+
+
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+    </body>
+
+</html>
